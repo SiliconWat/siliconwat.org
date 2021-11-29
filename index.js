23412262d6a442dbbb1d3a31d3b37470833e0085
Thon Ly <thonly@heartbank.ceo>
2021-11-29 10:39:26 -0800

    version 2

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function changeLanguage(event) {
     //console.log(event.target.value)
     document.location = event.target.value
 }
+
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5KRBGHZ5GV');
