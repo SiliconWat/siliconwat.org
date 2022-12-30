1265afd09a9bf43cd525a4dcfc1aad1d401fb864
Thon Ly <thonly@heartbank.ceo>
2022-12-29 17:08:23 -0800

    refactor

--- global.mjs
+++
@@ -1,2 +0,0 @@
-export const ORIGIN = window.location.hostname === '127.0.0.1' ? "http://127.0.0.1:5620" : "https://siliconwat.org";
-export const THONLY = window.location.hostname === '127.0.0.1' ? "http://127.0.0.1:5500" : "https://thonly.org";
