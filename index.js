ab392fc0ccab9328bf6b24ad61b016e9faffb3cf
Thon Ly <thonly@heartbank.ceo>
2022-12-30 16:13:38 -0800

    add sw-music

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import "./components/sw-main/sw-stealth/element.mjs";
 import "./components/sw-main/element.mjs";
 
 import "./components/sw-footer/element.mjs";
+import "https://frontend.siliconwat.com/components/sw-music/element.mjs";
+
+window.onload = () => document.querySelector('sw-music').shadowRoot.host.style.inset = "auto 50px 50px auto";
 
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
