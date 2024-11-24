552603326ef1340adcb547abf9515cd8549d928f
Thon Ly <thonly@siliconwat.dev>
2024-11-24 09:55:20 -0800

    major refactor

--- components/sw-footer/template.mjs
+++ components/sw-footer/template.mjs
@@ -14,15 +14,14 @@ template.innerHTML = `
                 <div>
                     <h5>Thank</h5>
                     <ul>
-                        <li><a href="https://dear.kiitos.earth/#SiliconWat">Kiitos</a></li>
+                        <li><a href="https://heartbank.ceo/#SiliconWat">HeartBank</a></li>
                     </ul> 
                 </div>
                 <div>
                     <h5>Donate</h5>
                     <ul>
-                        <li><a href="https://heartbank.fund/#SiliconWat">HeartBank</a></li>
                         <li><a href="https://github.com/sponsors/SiliconWat">GitHub</a></li>
-                        <li><a href="https://medium.com/@thonly/membership">Medium</a></li>
+                        <!--<li><a href="https://medium.com/@thonly/membership">Medium</a></li>-->
                     </ul>
                 </div>
             </section>
@@ -30,7 +29,6 @@ template.innerHTML = `
                 <div>
                     <h5>Follow</h5>
                     <ul>
-                        <li><a href="https://me.thonly.net/#SiliconWat">THonly</a></li>
                         <li><a href="https://www.facebook.com/siliconwat">Facebook</a></li>
                         <li><a href="https://medium.com/siliconwat">Medium</a>
                         <!--<li><a href="https://medium.com/@siliconwat">Medium</a></li>-->
